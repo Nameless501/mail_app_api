d54a5816e107cf69f7e1e89832735d8d84837e6c
Eugene Zorin <ezorin5@yandex.ru>
2023-06-09 01:08:25 +0400

    validation added

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,5 @@
 const BASE_FRONTEND_URL = 'https://task6frontend-production.up.railway.app';
 
-// const BASE_FRONTEND_URL = 'http://localhost:3000';
-
 const DEFAULT_ERROR_MESSAGE = 'Something went wrong. Try again Later.';
 
 module.exports = {
